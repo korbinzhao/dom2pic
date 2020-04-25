5c88e28cb49ebb4031204f0c9eda3cbbbb1cd7af
宛天 <zhaobin.zb@alibaba-inc.com>
2020-04-25 19:01:01 +0800

    update with async await

--- demo/index.tsx
+++ demo/index.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import Html2Img from '../src/index';
 
-// import {Table, Menu} from 'antd';
+import { Table, Menu } from 'antd';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
 
 import './index.less';
 
@@ -40,11 +43,45 @@ const App = () => {
     setTimeout(() => {
 
       doScreenshot();
-      
+
     }, 1000)
 
-   
+
   }, []);
+
+  const dataSource = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  ];
+
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
 
 
 
@@ -52,16 +89,17 @@ const App = () => {
     <div className="container">
       <div className="content2screenshot">
 
-        {/* <Table />
+        <Table dataSource={dataSource} columns={columns} />
 
-        <Menu /> */}
-
-        <img src="https://img.alicdn.com/tfs/TB1_ThFurj1gK0jSZFOXXc7GpXa-680-453.jpg" alt="" />
 
         hello world
 
-        <span>123</span>
-        <p>234</p>
+        <p>123</p>
+        <span className="text1">234</span>
+        <span className="text2">567</span>
+
+        <img src="https://img.alicdn.com/tfs/TB1_ThFurj1gK0jSZFOXXc7GpXa-680-453.jpg" alt="" />
+
 
       </div>
 
@@ -69,7 +107,10 @@ const App = () => {
 
       <img src={screenshot} alt="" />
 
+      <h2>--- svg ---</h2>
+
     </div>
+    
   );
 };
 
